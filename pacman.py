5293e6ef9b08b374d0fdbc49c66ccc01eb210963
awssilva <wssalexandre@gmail.com>
2023-01-07 19:34:27 -0300

    feat: find pacman in the map

--- pacman.py
+++ pacman.py
@@ -8,8 +8,21 @@ map = [
     "|--------|",
     "|G..|..G.|",
     "|...PP...|",
-    "|G....@|.|",
+    "|G...@.|.|",
     "|........|",
     "|--------|"
 
 ]
+
+def find_pacman(map):
+    pacman_x = -1
+    pacman_y = -1
+    for x in range(len(map)):
+        for y in range(len(map[x])):
+            if map[x][y] == '@':
+                pacman_x = x
+                pacman_y = y 
+
+    return pacman_x, pacman_y
+
+
