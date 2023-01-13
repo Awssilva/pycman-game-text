3d114e29cfcabab8cee950fb9c987cb393cdfbbb
awssilva <wssalexandre@gmail.com>
2023-01-13 19:46:58 -0300

    feat: pacman movement

--- pacman.py
+++ pacman.py
@@ -26,3 +26,16 @@ def find_pacman(map):
     return pacman_x, pacman_y
 
 
+def move_pacman(map, next_pacman_x, next_pacman_y):
+
+     pacman_x, pacman_y = find_pacman(map)
+    
+    #pacman to dot
+     everything_to_the_left = map[pacman_x][0:pacman_y]
+     everything_to_the_right = map[pacman_x][pacman_y+1:]
+     map[pacman_x] = everything_to_the_left + "." + everything_to_the_right
+    
+    #dot to pacman
+     everything_to_the_left = map[next_pacman_x][0:next_pacman_y]
+     everything_to_the_right = map[next_pacman_x][next_pacman_y+1:]
+     map[next_pacman_x] = everything_to_the_left + "@" + everything_to_the_right
